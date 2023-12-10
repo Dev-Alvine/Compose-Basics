4443d1fdac0dd3451df739aad96a51c7b75c719b
alvinemwa99 <alvinemwa99@gmail.com>
2023-12-10 13:07:49 +0300

    simple code lab

--- app/composecodelab/src/main/java/com/alvine/composecodelab/MainActivity.kt
+++ app/composecodelab/src/main/java/com/alvine/composecodelab/MainActivity.kt
@@ -3,13 +3,39 @@ package com.alvine.composecodelab
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.spring
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material.icons.filled.KeyboardArrowUp
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.alvine.composecodelab.ui.theme.ComposeBasicsTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,12 +43,10 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ComposeBasicsTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
+                    ) {
+                   SimpleApp(modifier = Modifier.fillMaxSize())
                 }
             }
         }
@@ -30,17 +54,133 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun SimpleApp(
+    modifier: Modifier= Modifier
+) {
+    var showOnLaunch by rememberSaveable {
+        mutableStateOf(true)
+    }
+    Surface(
+        modifier,
+        color = MaterialTheme.colorScheme.background
+    ) {
+        if (showOnLaunch){
+            LaunchScreen(onContinueClicked = {showOnLaunch=false})
+        }else
+        {
+            Greetings()
+        }
+    }
+}
+
+@Composable
+fun LaunchScreen(
+    onContinueClicked: () ->Unit,
+    modifier: Modifier=Modifier,
+) {
+    Column(
+        modifier=modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "Welcome to Basics CodeLab")
+        Button(modifier=modifier.padding(vertical = 24.dp),
+            onClick = onContinueClicked
+        ) {
+              Text(
+                  text = "Continue ",
+                  style = MaterialTheme.typography.bodyMedium,
+                  fontWeight = FontWeight.Bold
+              )
+        }
+    }
+}
+
+@Composable
+private fun Greetings(
+    modifier: Modifier=Modifier,
+    names:List<String> = List(500){"$it"}
+) {
+    LazyColumn(
+        modifier=modifier.padding(vertical = 4.dp)
+    ){
+        items(items =names){ name ->
+            Greeting(name = name)
+
+        }
+    }
+}
+
+@Composable
+fun Greeting(
+    name: String,
+) {
+    Card(
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.primary
+        ),
+        modifier= Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+    ) {
+        CardContent(name)
+    }
+}
+
+@Composable
+private fun CardContent (
+    name: String
+) {
+    var expanded by remember{
+        mutableStateOf(false)
+    }
+    Row (
+        modifier = Modifier
+            .padding(12.dp)
+            .animateContentSize(
+                animationSpec = spring(
+                    dampingRatio = Spring.DampingRatioMediumBouncy,
+                    stiffness = Spring.StiffnessLow
+                )
+            )
+    ){
+        Column(
+            modifier= Modifier
+                .weight(1f)
+                .padding(12.dp)
+        ) {
+            Text(text = "Hello, ")
+            Text(text = name, style = MaterialTheme.typography.headlineMedium.copy(
+                fontWeight = FontWeight.ExtraBold
+            )
+            )
+            if(expanded){
+                Text(text = ("This codelab covers the following:\r\n" +
+                             "The art of reusing Composables\r\n"+
+                             "State and State Hoisting\r\n" +
+                            "List Animation\r\n"  +
+                            "Layout and Styling"
+                        ))
+            }
+
+        }
+        IconButton(
+            onClick = { expanded=!expanded }
+        ) {
+            Icon(
+                imageVector = if (expanded) Icons.Filled.KeyboardArrowUp else Icons.Filled.KeyboardArrowDown,
+                contentDescription = if (expanded){
+                    stringResource(R.string.show_less)
+                }else{
+                    stringResource(R.string.show_more)
+                }
+            )
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     ComposeBasicsTheme {
-        Greeting("Android")
+        SimpleApp()
     }
 }
