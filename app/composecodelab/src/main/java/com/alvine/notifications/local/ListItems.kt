722f7c7d7f055e329e58049df6cc628dafb108a5
Dev-Alvine <alvinemwa99@gmail.com>
2024-06-14 22:34:32 +0300

    updated

---
+++ app/composecodelab/src/main/java/com/alvine/notifications/local/ListItems.kt
@@ -0,0 +1,37 @@
+package com.alvine.notifications.local
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.core.app.NotificationCompat
+
+@Composable
+fun ListItems(
+    modifier: Modifier = Modifier,
+    data: List<ComposeRandomItem> = DataSource.plants.map { it }
+) {
+    LazyColumn(modifier = modifier.padding(vertical = 4.dp)) {
+        items(items = data.toMutableList()) { n ->
+            ComposeCard(
+                name = n.name,
+                type = n.type,
+                description = n.description
+            )
+        }
+    }
+
+    val notification = NotificationCompat.Builder(this, CHANNEL_ID)
+        .setContentTitle("2 new messages with $sender")
+        .setContentText(subject)
+        .setSmallIcon(R.drawable.new_message)
+        .setStyle(
+            NotificationCompat.MessagingStyle(user)
+            .addMessage(messages[1].getText(), messages[1].getTime(), messages[1].getPerson())
+            .addMessage(messages[2].getText(), messages[2].getTime(), messages[2].getPerson())
+        )
+        .build()
+
+}
+
