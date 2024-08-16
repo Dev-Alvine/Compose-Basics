071106b07edad36779f3c012a72060fbfcfe3442
Dev-Alvine <alvinemwa99@gmail.com>
2024-08-16 21:53:31 +0300

    refactored

--- app/composecodelab/src/main/java/com/alvine/gestures/GesturesFile.kt
+++ app/composecodelab/src/main/java/com/alvine/gestures/GesturesFile.kt
@@ -1,25 +1,38 @@
 package com.alvine.gestures
 
-import androidx.annotation.DrawableRes
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.clickable
+import androidx.compose.foundation.background
+import androidx.compose.foundation.combinedClickable
 import androidx.compose.foundation.gestures.detectTapGestures
+import androidx.compose.foundation.gestures.detectTransformGestures
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.lazy.grid.rememberLazyGridState
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.CheckCircle
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clipToBounds
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusProperties
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.TransformOrigin
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import coil.compose.rememberAsyncImagePainter
 import com.alvine.composecodelab.R
@@ -27,12 +40,24 @@ import com.alvine.composecodelab.R
 
 @Composable
 fun App(
-    photos:List<Photos>
+    photos:List<Photo>
 ) {
     var activeId by rememberSaveable {
         mutableStateOf<Int?>(null)
     }
-    PhotoGrid(photos = photos)
+    val gridState= rememberLazyGridState()
+    var autoScrollSpeed by remember {
+        mutableStateOf(0f)
+    }
+    val scrim= remember(activeId){FocusRequester()}
+
+    PhotoGrid(
+        photos = photos,
+        state = gridState,
+        setAutoScrolledSpeed = { autoScrollSpeed = it },
+        navigateToPhoto={activeId=it},
+        modifier=Modifier.focusProperties { canFocus=activeId == nulll }
+    )
     if (activeId!=null){
         FullScreenPhoto(
             photo= photos.first{it.id==activeId},
@@ -42,8 +67,8 @@ fun App(
 }
 
 @Composable
-fun FullScreenPhoto(
-    photo: Photos,
+private fun FullScreenPhoto(
+    photo: Photo,
     onDismiss: () -> Unit,
     modifier: Modifier=Modifier
 ) {
@@ -51,139 +76,209 @@ fun FullScreenPhoto(
         modifier.fillMaxSize(),
         contentAlignment = Alignment.Center
     ){
-        //Scrim()
+        Scrim(onDismiss)
         PhotoImage(photo)
     }
 
 }
 
 @Composable
+private fun Scrim(
+    modifier: Modifier=Modifier,
+    onClose:() -> Unit
+) {
+    Box (
+        modifier
+            .fillMaxSize()
+            .background(Color.DarkGray.copy(alpha = 0.75f))
+            .pointerInput(onClose()) {
+                detectTapGestures { onClose() }
+            }
+    ){
+
+    }
+}
+
+@Composable
 private fun PhotoImage(
-    photo: Photos,
+    photo: Photo,
     modifier: Modifier=Modifier,
 
-) {
+)  {
+    var offset by remember { mutableStateOf(Offset.Zero) }
+    var zoom by remember { mutableStateOf(1f) }
   Image(
       painter = rememberAsyncImagePainter(photo.highResUrl),
       contentDescription ="",
       modifier= modifier
           .clipToBounds()
-          .pointerInput(Unit){
-              detectTapGestures(onDoubleTap = {tapOffset->
-                  zoom= if (zoom > 1f) 1f else 2f
-                  offset= calculateDoubleTapOffset(zoom, size,tapOffset)
+          .pointerInput(Unit) {
+              detectTapGestures(onDoubleTap = { tapOffset ->
+                  zoom = if (zoom > 1f) 1f else 2f
+                  offset = calculateDoubleTapOffset(zoom, size, tapOffset)
 
               })
           }
+          .pointerInput(Unit) {
+              detectTransformGestures { centroid, pan, zoom, gestureZoom ->
+                  offset = offset.calculateNewOffset(
+                      centroid, pan, zoom, gestureZoom, size
+                  )
+                  zoom = maxOf(1f, zoom * gestureZoom)
+              }
+          }
           .graphicsLayer {
-              translationX= -offset.x*zoom
-              translationY-offset.y*zoom
-              scaleX= zoom; scaleY=zoom
-              transformOrigin= TransformOrigin(0f,0f)
-          } )
+              translationX = -offset.x * zoom
+              translationY - offset.y * zoom
+              scaleX = zoom; scaleY = zoom
+              transformOrigin = TransformOrigin(0f, 0f)
+          }
+          .aspectRatio(1f)
+
+  )
 }
 
 
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 private fun PhotoGrid(
-    photos:List<Photos>,
+    photos:List<Photo>,
     navigateToPhoto:(Int) ->Unit={}
 ) {
+    var selectedIds by rememberSaveable {
+        mutableStateOf(emptyList<Int>())
+    }
+    val inSelectionMode by remember {
+        derivedStateOf { selectedIds.isNotEmpty() }
+    }
     LazyVerticalGrid(GridCells.Adaptive(128.dp)) {
         items(photos, key = {it.id!!}){photo->
-            PhotoItem(
 
-data class Photos(
-    val id:Int?=null,
-    @DrawableRes val image:Int
-) {
-    val highResUrl: Any?
-        get() {
-            TODO()
+            val selected by remember {
+                derivedStateOf { photo.id in selectedIds }
+            }
+
+            PhotoItem(
+                selected,inSelectionMode,
+                Modifier.combinedClickable(
+                    onClick = {navigateToPhoto(photo.id)},
+                    onLongClick = {selectedIds +=photo.id}
+                )
+            )
         }
+    }
 }
 
-val images= listOf(
-    Photos(
-        image = R.drawable.one
-    ),
-      Photos(
-        image = R.drawable.two
-    ),
-      Photos(
-        image = R.drawable.three
-    ),
-      Photos(
-        image = R.drawable.four
-    ),
-      Photos(
-        image = R.drawable.five
-    ),
-      Photos(
-        image = R.drawable.six
-    ),
-      Photos(
-        image = R.drawable.seven
-    ),
-      Photos(
-        image = R.drawable.eight
-    ),
-      Photos(
-        image = R.drawable.nine
-    ),
-      Photos(
-        image = R.drawable.ten
-    ),
-      Photos(
-        image = R.drawable.eleven
-    ),
-      Photos(
-        image = R.drawable.twelve
-    ),
-      Photos(
-        image = R.drawable.thirteen
-    ),
-      Photos(
-        image = R.drawable.fourteen
-    ),
-      Photos(
-        image = R.drawable.fifteen
-    ),
-    Photos(
-        image = R.drawable.five
-    ),
-    Photos(
-        image = R.drawable.six
-    ),
-    Photos(
-        image = R.drawable.seven
-    ),
-    Photos(
-        image = R.drawable.eight
-    ),
-    Photos(
-        image = R.drawable.nine
-    ),
-    Photos(
-        image = R.drawable.ten
-    ),
-    Photos(
-        image = R.drawable.eleven
-    ),
-    Photos(
-        image = R.drawable.twelve
-    ),
-    Photos(
-        image = R.drawable.thirteen
-    ),
-    Photos(
-        image = R.drawable.fourteen
-    ),
-    Photos(
-        image = R.drawable.fifteen
-    ),
+@Composable
+fun PhotoItem(selected: Boolean, inSelectionMode: Boolean, combinedClickable: Modifier) {
+    if (inSelectionMode){
+        if (selected){
+            Icons.Filled.CheckCircle
+        }else{
+            Image(
+                painter = painterResource(
+                     id = R.drawable.baseline_radio_button_unchecked_24),
+                contentDescription = ""
+            )
+        }
+    }
+
+}
+
+class Photo(
+    val id: Int,
+    val url:String,
+    val highResUrl:String,
+    val description:String
+)
+
+//
+//data class Photos(
+//    val id:Int?=null,
+//    @DrawableRes val image:Int
+//)
+//
+//val images= listOf(
+//    Photos(
+//        image = R.drawable.one
+//    ),
+//      Photos(
+//        image = R.drawable.two
+//    ),
+//      Photos(
+//        image = R.drawable.three
+//    ),
+//      Photos(
+//        image = R.drawable.four
+//    ),
+//      Photos(
+//        image = R.drawable.five
+//    ),
+//      Photos(
+//        image = R.drawable.six
+//    ),
+//      Photos(
+//        image = R.drawable.seven
+//    ),
+//      Photos(
+//        image = R.drawable.eight
+//    ),
+//      Photos(
+//        image = R.drawable.nine
+//    ),
+//      Photos(
+//        image = R.drawable.ten
+//    ),
+//      Photos(
+//        image = R.drawable.eleven
+//    ),
+//      Photos(
+//        image = R.drawable.twelve
+//    ),
+//      Photos(
+//        image = R.drawable.thirteen
+//    ),
+//      Photos(
+//        image = R.drawable.fourteen
+//    ),
+//      Photos(
+//        image = R.drawable.fifteen
+//    ),
+//    Photos(
+//        image = R.drawable.five
+//    ),
+//    Photos(
+//        image = R.drawable.six
+//    ),
+//    Photos(
+//        image = R.drawable.seven
+//    ),
+//    Photos(
+//        image = R.drawable.eight
+//    ),
+//    Photos(
+//        image = R.drawable.nine
+//    ),
+//    Photos(
+//        image = R.drawable.ten
+//    ),
+//    Photos(
+//        image = R.drawable.eleven
+//    ),
+//    Photos(
+//        image = R.drawable.twelve
+//    ),
+//    Photos(
+//        image = R.drawable.thirteen
+//    ),
+//    Photos(
+//        image = R.drawable.fourteen
+//    ),
+//    Photos(
+//        image = R.drawable.fifteen
+//    ),)
 
 
-    )
+
 
 
